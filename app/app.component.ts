543ec53539dad667bc5442faefaf3efa91c136da
Aftab Ansari <aftabansari10662@gmail.com>
2017-10-14 15:07:02 +0530

    Routing Done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core'
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
-import { Movie } from "./movie.model"
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app/app.component.html'
 })
 
-@Injectable()
 export class AppComponent {
-  name: string;
-  movies: any;
-  model:Movie;
-  constructor(private httpClient: HttpClient) {
-    this.name = "Aftab Ansari"
-    this.model = new Movie("","");
-    this.httpClient.get("app/movies.json")
-      .subscribe(data => {
-        // Read the result field from the JSON response.
-        this.movies = data;
-        console.log(this.movies);
-      });
-  }
-
-  createMovie(formValues) {
-    console.log(formValues.value);
-  }
+ 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,33 @@ import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+import { MovieListComponent } from "./movie-list.component";
+import { CreateMovieComponet } from "./movie-create.component"
 
 @NgModule({
     imports:
     [
         BrowserModule,
         HttpClientModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'home',
+                component: AppComponent
+            },
+            {
+                path: "createMovies",
+                component: CreateMovieComponet
+            },
+            {
+                path: "movies",
+                component: MovieListComponent
+            }
+        ])
     ],
-    declarations: [AppComponent],
+    declarations: [AppComponent, CreateMovieComponet, MovieListComponent],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
