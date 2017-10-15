00f6503323c35e14fd74022fb0f7066b933a2a5c
Aftab Ansari <aftabansari10662@gmail.com>
2017-10-15 13:15:47 +0530

    Details route

--- app/movie-detail.component.ts
+++ app/movie-detail.component.ts
@@ -2,21 +2,25 @@ import { Router } from "@angular/router";
 
 import { Component, Injectable, OnInit } from "@angular/core";
 
+import { ActivatedRoute } from "@angular/router";
+
 @Component(
     {
-        templateUrl:"./app/movie-detail.component.html",
+        templateUrl: "./app/movie-detail.component.html",
 
     }
 )
 
 @Injectable()
 export class MovieDetailsComponent implements OnInit {
-
-    constructor(private _router: Router) {
+    movieId: string;
+    constructor(
+        private _router: Router,
+        private _activatedRouter: ActivatedRoute) {
 
     }
     ngOnInit() {
-
+        this.movieId = this._activatedRouter.snapshot.paramMap.get("id");
 
     }
 
