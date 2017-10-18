e4bb005592549c28b1baa4d901f31eae61be585d
Aftab Ansari <aftabansari10662@gmail.com>
2017-10-18 16:09:19 +0530

    Movie Details Page

--- app/movie-detail.component.ts
+++ app/movie-detail.component.ts
@@ -4,6 +4,9 @@ import { Component, Injectable, OnInit } from "@angular/core";
 
 import { ActivatedRoute } from "@angular/router";
 
+import { MovieData } from "./movie.service";
+import { Movie } from "./movie.model";
+
 @Component(
     {
         templateUrl: "./app/movie-detail.component.html",
@@ -14,15 +17,24 @@ import { ActivatedRoute } from "@angular/router";
 @Injectable()
 export class MovieDetailsComponent implements OnInit {
     movieId: string;
+    movie: Movie;
     constructor(
         private _router: Router,
-        private _activatedRoute: ActivatedRoute) {
+        private _activatedRoute: ActivatedRoute,
+        private _movieData: MovieData) {
         this._activatedRoute.params.subscribe(p => this.loadMovie(p["id"]));
 
     }
 
     loadMovie(movieId: any) {
         this.movieId = movieId;
+        this._movieData.getById(this.movieId)
+            .subscribe((movie: Movie) => {
+                this.movie = movie;
+            },
+            (error) => {
+                console.log(error);
+            })
     }
 
     ngOnInit(): void {
